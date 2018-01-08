598ce9f61bcb9c9ba35fa922ba9b67851d9e8800
vitracao <duccm.vitracao@gmail.com>
2018-01-08 18:20:59 +0700

    PC get job

--- app/Http/Controllers/JobEditController.php
+++ app/Http/Controllers/JobEditController.php
@@ -11,8 +11,37 @@ class JobEditController extends Controller
 {
     public function index(){
         
-        $jobs = Job::all();
-        return view('editor.index',compact('jobs'));
-
+        return $this->getJob();
+        //return view('editor.index',compact('jobs'));
     }
+
+    public function getJob(){
+        $jobStatus = $this->findAvailableJob();
+        $jobStatus->status = "Đang edit";
+        return $jobStatus;
+    }
+
+    public function findAvailableJob(){
+
+        $jobStatus = new JobStatus;
+
+        if($this->findPrioritizeJob($jobStatus)){
+            return $this->findPrioritizeJob($jobStatus);
+        }else if(!$this->findQueuedJob($jobStatus)){
+            return $this->findQueuedJob($jobStatus);
+        }else{
+            return NULL;
+        }
+    }
+
+    public function findPrioritizeJob($jobStatus){
+        
+        return $jobStatus->where('status','Ưu tiên')->first();
+    }
+
+    public function findQueuedJob($jobStatus){
+        
+        return $jobStatus->where('status','Trong hàng đợi')->first();
+    }
+
 }
